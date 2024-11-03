ca132c9820bd2a21f680cc59226c6066e2dd4348
diartor <diartor.dev@gmail.com>
2024-11-03 23:01:37 +0330

    bugfix: idle

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from bot.managers.callback import CallbackHandler
 from bot.managers.nickname import NicknameManager
 from bot.managers.start import StartBot
 
-
 bot = TeleBot(token=config('BOT_TOKEN', cast=str))
 
 start_bot = StartBot(bot)
@@ -22,10 +21,13 @@ bot.register_message_handler(start_bot.link, commands=['link'])
 bot.register_message_handler(nickname_handler.set_nickname, commands=['nickname'])
 bot.register_message_handler(chat_handler.anonymous_chat, content_types=['text'])
 bot.register_callback_query_handler(callback_handler.handle_callback, func=lambda call: True)
+
+
 def keep_alive():
     while True:
         bot.get_me()  # Pings the API to keep the connection alive
         time.sleep(60)  # Sends a ping every 60 seconds
+
 
 # Start the keep-alive thread
 threading.Thread(target=keep_alive, daemon=True).start()
