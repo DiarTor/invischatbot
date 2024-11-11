2fd8ea49f2afb3fc8d6964be6d326888ac5c7a48
diartor <diartor.dev@gmail.com>
2024-11-11 20:21:35 +0330

    feat: status admin command

---
+++ bot/admin/bot_administarion.py
@@ -0,0 +1,100 @@
+from datetime import datetime, timedelta
+
+from telebot.async_telebot import AsyncTeleBot
+from telebot.types import Message
+
+from bot.utils.database import users_collection
+from bot.utils.language import get_response
+
+
+class AdminManager:
+    def __init__(self, bot: AsyncTeleBot):
+        self.bot = bot
+
+    async def get_bot_status(self, msg: Message):
+        if not msg.from_user.id == int(1154909190):
+            return
+        user_counts = self.get_users_count()
+        chat_counts = self.get_chat_counts()
+        # Send the status message
+        await self.bot.send_message(msg.chat.id,
+                                    get_response('admin.status', user_counts['today'], user_counts['this_week'],
+                                                 user_counts['this_month'], user_counts['this_year'],
+                                                 user_counts['all_time'], chat_counts['today'],
+                                                 chat_counts['this_week'], chat_counts['this_month'],
+                                                 chat_counts['this_year'], chat_counts['all_time']),
+                                    parse_mode='Markdown')
+
+    @staticmethod
+    def get_users_count():
+        # Get the current datetime and convert to UNIX timestamp
+        now = datetime.now()
+
+        # Calculate the start times as UNIX timestamps
+        start_today = datetime(now.year, now.month, now.day).timestamp()
+        start_week = (datetime(now.year, now.month, now.day) - timedelta(
+            days=now.weekday())).timestamp()  # Start of the current week (Monday)
+        start_month = datetime(now.year, now.month, 1).timestamp()  # First day of the current month
+        start_year = datetime(now.year, 1, 1).timestamp()  # First day of the current year
+
+        # Query the MongoDB collection to count users in each timeframe
+        all_time = users_collection.count_documents({})
+        today_count = users_collection.count_documents({"joined_at": {"$gte": start_today}})
+        week_count = users_collection.count_documents({"joined_at": {"$gte": start_week}})
+        month_count = users_collection.count_documents({"joined_at": {"$gte": start_month}})
+        year_count = users_collection.count_documents({"joined_at": {"$gte": start_year}})
+
+        return {
+            'all_time': all_time,
+            'today': today_count,
+            'this_week': week_count,
+            'this_month': month_count,
+            'this_year': year_count
+        }
+
+    @staticmethod
+    def get_chat_counts():
+        # Current datetime
+        now = datetime.now()
+
+        # Define the start times for each period
+        start_of_today = datetime(now.year, now.month, now.day)
+        start_of_week = start_of_today - timedelta(days=start_of_today.weekday())
+        start_of_month = datetime(now.year, now.month, 1)
+        start_of_year = datetime(now.year, 1, 1)
+
+        # Convert the start times to timestamps
+        timestamp_today = start_of_today.timestamp()
+        timestamp_week = start_of_week.timestamp()
+        timestamp_month = start_of_month.timestamp()
+        timestamp_year = start_of_year.timestamp()
+
+        # Initialize counters
+        counts = {
+            'today': 0,
+            'this_week': 0,
+            'this_month': 0,
+            'this_year': 0,
+            'all_time': 0
+        }
+
+        # Retrieve all user documents
+        user_documents = users_collection.find()
+
+        # Loop through each user document and count chats based on 'chat_created_at' timestamp
+        for user_doc in user_documents:
+            for chat in user_doc.get("chats", []):
+                chat_created_at = chat.get("chat_created_at")
+                if chat_created_at is not None:
+                    # Increment counts based on time periods
+                    counts['all_time'] += 1
+                    if chat_created_at >= timestamp_year:
+                        counts['this_year'] += 1
+                    if chat_created_at >= timestamp_month:
+                        counts['this_month'] += 1
+                    if chat_created_at >= timestamp_week:
+                        counts['this_week'] += 1
+                    if chat_created_at >= timestamp_today:
+                        counts['today'] += 1
+
+        return counts
--- main.py
+++ main.py
@@ -8,20 +8,32 @@ from bot.managers.block_user import BlockUserManager
 from bot.managers.callback import CallbackHandler
 from bot.managers.nickname import NicknameManager
 from bot.managers.start import StartBot
-
+from bot.admin.bot_administarion import AdminManager
 bot = AsyncTeleBot(token=config('BOT_TOKEN', cast=str), colorful_logs=True)
 
+# Bot Commands Classes
 start_bot = StartBot(bot)
 chat_handler = ChatHandler(bot)
 callback_handler = CallbackHandler(bot)
 nickname_handler = NicknameManager(bot)
 block_user_handler = BlockUserManager(bot)
-# Register handlers
+
+# Admin Commands Classes
+admin_handler = AdminManager(bot)
+
+# Bot Commands
 bot.register_message_handler(start_bot.start, commands=['start'])
 bot.register_message_handler(start_bot.link, commands=['link'])
 bot.register_message_handler(nickname_handler.set_nickname, commands=['nickname'])
 bot.register_message_handler(block_user_handler.block_list, commands=['blocklist'])
+
+# Admin Commands
+bot.register_message_handler(admin_handler.get_bot_status, commands=['status'])
+
+# Content Type Handlers
 bot.register_message_handler(chat_handler.anonymous_chat, content_types=['text'])
+
+# CallBack Handlers
 bot.register_callback_query_handler(callback_handler.handle_callback, func=lambda call: True)
 
 if __name__ == '__main__':
