8d797a78636bcb64de18d4ef42785a8fb31addad
diartor <diartor.dev@gmail.com>
2024-11-18 02:28:01 +0330

    feat: send voice, video note, audio, document

--- bot/managers/chat.py
+++ bot/managers/chat.py
@@ -74,6 +74,8 @@ class ChatHandler:
             await self._handle_document(msg)
         elif msg.video_note:
             await self._handle_video_note(msg)
+        elif msg.voice:
+            await self._handle_voice(msg)
         else:
             await self.bot.send_message(msg.chat.id, get_response('errors.unknown_media'))
 
@@ -105,6 +107,10 @@ class ChatHandler:
         """Handle video note messages."""
         await self._process_chat(msg, is_video_note=True)
 
+    async def _handle_voice(self, msg: Message):
+        """Handle voice messages."""
+        await self._process_chat(msg, is_voice=True)
+
     async def _process_chat(self, msg: Message, **kwargs):
         """Process the chat based on the type of message."""
         user_chat = get_user(msg.from_user.id)
@@ -129,37 +135,54 @@ class ChatHandler:
         """Forward media to the recipient based on message type."""
         try:
             if kwargs.get('is_sticker'):
-                await self.bot.send_message(recipient_id, get_response("texting.sending.sticker.recipient",
-                                                                       sender_anny_id, ),
-                                            parse_mode='Markdown',
+                await self.bot.send_sticker(recipient_id, msg.sticker.file_id,
                                             reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
                                                                                                      msg.id))
-                await self.bot.send_sticker(recipient_id, msg.sticker.file_id)
             elif kwargs.get('is_photo'):
                 await self.bot.send_photo(recipient_id, msg.photo[-1].file_id,
-                                          caption=get_response('texting.sending.photo.recipient',
+                                          caption=get_response('texting.sending.text.recipient',
                                                                caption, sender_anny_id),
                                           parse_mode='Markdown',
                                           reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
                                                                                                    msg.id))
             elif kwargs.get('is_video'):
                 await self.bot.send_video(recipient_id, msg.video.file_id,
-                                          caption=get_response('texting.sending.video.recipient', caption,
+                                          caption=get_response('texting.sending.text.recipient', caption,
                                                                sender_anny_id),
-                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id, ),
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id),
+                                          parse_mode='Markdown')
+            elif kwargs.get('is_voice'):
+                await self.bot.send_voice(recipient_id, msg.voice.file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id),
                                           parse_mode='Markdown')
             elif kwargs.get('is_video_note'):
-                await self.bot.send_video_note(recipient_id, msg.video_note.file_id)
+                await self.bot.send_video_note(recipient_id, msg.video_note.file_id,
+                                               reply_markup=KeyboardMarkupGenerator().recipient_buttons(
+                                                   sender_anny_id, msg.id))
             elif kwargs.get('is_audio'):
-                await self.bot.send_audio(recipient_id, msg.audio.file_id)
+                await self.bot.send_audio(recipient_id, msg.audio.file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id),
+                                          parse_mode='Markdown')
             elif kwargs.get('is_document'):
-                await self.bot.send_document(recipient_id, msg.document.file_id)
+                await self.bot.send_document(recipient_id, msg.document.file_id,
+                                             caption=get_response('texting.sending.text.recipient', caption,
+                                                                  sender_anny_id),
+                                             reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                      msg.id),
+                                             parse_mode='Markdown')
             else:
                 await self.bot.send_message(recipient_id, get_response("texting.sending.text.recipient", msg.text,
-                                                                       get_user(msg.chat.id).get('id')),
+                                                                       sender_anny_id),
                                             parse_mode='Markdown',
                                             reply_markup=KeyboardMarkupGenerator().recipient_buttons(
-                                                get_user(msg.chat.id).get('id'), msg.id))
+                                                sender_anny_id, msg.id))
 
             # After sending, update the chat state (if applicable)
             close_existing_chats(msg.chat.id)
@@ -173,22 +196,89 @@ class ChatHandler:
         recipient_id, original_message_id = user_chat['reply_target_user_id'], user_chat['reply_target_message_id']
         recipient_user = users_collection.find_one({"id": recipient_id})
 
-        if recipient_user:
-            if not is_user_blocked(user_chat.get("id"), recipient_user.get('user_id')):
-                try:
-                    await self._send_reply(msg, recipient_user['user_id'], original_message_id, user_chat['id'])
-                except ApiTelegramException:
-                    self._handle_bot_blocked(msg)
-                    return
-            else:
-                reset_replying_state(msg.chat.id)
-                await self.bot.send_message(msg.chat.id, get_response('blocking.blocked_by_user'),
-                                            reply_markup=KeyboardMarkupGenerator().main_buttons())
-                return
-            await self.bot.send_message(
-                msg.chat.id, get_response('texting.replying.sent'), parse_mode='Markdown',
-                reply_markup=KeyboardMarkupGenerator().main_buttons())
-            reset_replying_state(msg.from_user.id)
+        if not recipient_user:
+            reset_replying_state(msg.chat.id)
+            await self.bot.send_message(msg.chat.id, get_response('errors.user_not_found'),
+                                        reply_markup=KeyboardMarkupGenerator().main_buttons())
+            return
+
+        if is_user_blocked(user_chat.get("id"), recipient_user.get('user_id')):
+            reset_replying_state(msg.chat.id)
+            await self.bot.send_message(msg.chat.id, get_response('blocking.blocked_by_user'),
+                                        reply_markup=KeyboardMarkupGenerator().main_buttons())
+            return
+
+        try:
+            # Check the type of message and forward appropriately
+            caption = msg.caption if msg.caption else ""
+            sender_anny_id = user_chat.get('id')
+            if msg.photo:
+                await self.bot.send_photo(recipient_user['user_id'], msg.photo[-1].file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_to_message_id=original_message_id,
+                                          parse_mode='Markdown',
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id))
+            elif msg.video:
+                await self.bot.send_video(recipient_user['user_id'], msg.video.file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_to_message_id=original_message_id,
+                                          parse_mode='Markdown',
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id))
+            elif msg.document:
+                await self.bot.send_document(recipient_user['user_id'], msg.document.file_id,
+                                             caption=get_response('texting.sending.text.recipient', caption,
+                                                                  sender_anny_id),
+                                             reply_to_message_id=original_message_id,
+                                             parse_mode='Markdown',
+                                             reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                      msg.id))
+            elif msg.audio:
+                await self.bot.send_audio(recipient_user['user_id'], msg.audio.file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_to_message_id=original_message_id,
+                                          parse_mode='Markdown',
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id))
+            elif msg.voice:
+                await self.bot.send_voice(recipient_user['user_id'], msg.voice.file_id,
+                                          caption=get_response('texting.sending.text.recipient', caption,
+                                                               sender_anny_id),
+                                          reply_to_message_id=original_message_id,
+                                          parse_mode='Markdown',
+                                          reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                   msg.id))
+            elif msg.video_note:
+                await self.bot.send_video_note(recipient_user['user_id'], msg.video_note.file_id,
+                                               reply_to_message_id=original_message_id,
+                                               reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                        msg.id))
+            elif msg.sticker:
+                await self.bot.send_sticker(recipient_user['user_id'], msg.sticker.file_id,
+                                            reply_to_message_id=original_message_id,
+                                            reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                     msg.id))
+            else:  # Default to text
+                await self.bot.send_message(recipient_user['user_id'],
+                                            get_response('texting.sending.text.recipient', msg.text, sender_anny_id),
+                                            reply_to_message_id=original_message_id,
+                                            parse_mode='Markdown',
+                                            reply_markup=KeyboardMarkupGenerator().recipient_buttons(sender_anny_id,
+                                                                                                     msg.id))
+        except ApiTelegramException:
+            self._handle_bot_blocked(msg)
+            return
+
+        # Notify sender that reply was sent successfully
+        await self.bot.send_message(
+            msg.chat.id, get_response('texting.replying.sent'), parse_mode='Markdown',
+            reply_markup=KeyboardMarkupGenerator().main_buttons()
+        )
+        reset_replying_state(msg.from_user.id)
 
     async def handle_link(self):
         await LinkManager(self.bot).link(self.msg)
@@ -282,7 +372,10 @@ class ChatHandler:
         ).get('chats', [None])[0]
 
     def _handle_bot_blocked(self, msg: Message):
-        self.bot.send_message(msg.chat.id, get_response('errors.bot_blocked'))
+        close_existing_chats(msg.chat.id)
+        reset_replying_state(msg.chat.id)
+        self.bot.send_message(msg.chat.id, get_response('errors.bot_blocked'),
+                              reply_markup=KeyboardMarkupGenerator().main_buttons())
 
     @staticmethod
     def _update_user_field(user_id, field, value):
