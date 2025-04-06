30072817f7d9ffa38386afccecd81ff05d49488b
diartor <diartor.dev@gmail.com>
2025-04-07 01:14:04 +0330

    ref: minor changes

--- bot/admin/keyboard.py
+++ bot/admin/keyboard.py
@@ -6,6 +6,7 @@ from bot.common.keyboard import KeyboardMarkupGenerator
 class Keyboard(KeyboardMarkupGenerator):
 
     def main_panel(self):
-        buttons = [[InlineKeyboardButton('💬 آمار چت ها', callback_data='admin-chats_stats')],[InlineKeyboardButton('👥 آمار کاربران', callback_data='admin-users_stats')]]
+        buttons = [[InlineKeyboardButton('💬 آمار چت ها', callback_data='admin-chats_stats'),
+                    InlineKeyboardButton('👥 آمار کاربران', callback_data='admin-users_stats')]]
 
         return self._create_list_inline_keyboard(buttons)
